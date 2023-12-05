b72c72f5bdae749a9ed61dd9fdbe90400dfe4837
mrgw454 <ron@kdomain.org>
2023-12-05 15:11:28 -0600

    install GreaseWeazle

--- scripts/fix-cocopi.sh
+++ scripts/fix-cocopi.sh
@@ -409,6 +409,25 @@ else
 fi
 
 
+# install GreaseWeazle
+# check for fix
+fix="fix-20231205-02"
+if grep -q "$fix" $file; then
+    echo fix $fix already complete.
+    echo
+else
+    echo Applying fix $fix...
+    echo
+
+	python3 -m pip install git+https://github.com/keirf/greaseweazle@latest
+
+    cd $HOME
+
+    echo "$fix" >>$file
+    echo
+fi
+
+
 echo
 echo
 echo Please reboot as soon as possible so all updates can be applied.  Thank you.
